123d982be88f911b786dd5ebcc38304d9d71a869
eleocraft <eliseo.cailloux@gmx.de>
2025-12-31 16:56:57 +0100

    editing gitignore

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use simple_config::Config;
 async fn main() {
     let mut config = DBConfig::new();
     config.parse_file("db.conf").expect("could not parse config file");
-    config.parse_cli().expect("could not parse db");
+    config.parse_cli().expect("could not parse cli args");
 
     database::run(config).await
         .expect("database service finished with non zero exit code");
