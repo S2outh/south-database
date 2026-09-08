3016f46dceb78b3102bbe31b298b342006ff4fe2
eleocraft <eliseo.cailloux@gmx.de>
2026-09-08 03:45:48 +0200

    upd

--- flake.nix
+++ flake.nix
@@ -3,50 +3,44 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    fenix = {
-      url = "github:nix-community/fenix/monthly";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    systems.url = "github:nix-systems/default";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    fenix.url = "github:nix-community/fenix/monthly";
     naersk = {
       url = "github:nix-community/naersk";
-      inputs.nixpkgs.follows = "nixpkgs";
       inputs.fenix.follows = "fenix";
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, fenix, naersk }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } ({ ... }: {
+      systems = import inputs.systems;
+      perSystem = { pkgs, system, inputs', ... }: 
       let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            fenix.overlays.default 
-          ];
-        };
-        rust-toolchain = pkgs.fenix.complete.toolchain;
-      in
-      {
+        fpkgs = inputs'.fenix.packages;
+        rust-toolchain = fpkgs.stable.toolchain;
+      in {
         devShells.default =
         pkgs.mkShell {
-          buildInputs = with pkgs; [
+          nativeBuildInputs = with pkgs; [
             rust-toolchain
-            rust-analyzer-nightly
+            rust-analyzer
 
             # extra cargo tools
             cargo-edit
             cargo-expand
+            cargo-show-asm
+            cargo-binutils
           ];
 
           # set the rust src for rust_analyzer
           RUST_SRC_PATH = "${rust-toolchain}/lib/rustlib/src/rust/library";
         };
-
         packages.default = 
         let
           cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
         in
-        (naersk.lib.${system}.override {
+        (inputs.naersk.lib.${system}.override {
           cargo = rust-toolchain;
           rustc = rust-toolchain;
         }).buildPackage {
@@ -56,6 +50,7 @@
             meta = (old.meta or {}) // { mainProgram = cargoToml.package.name; };
           };
         };
-      }
-    );
+      };
+    });
 }
+
